ccfa1049f3163f38c6f4d4912215fc410a1b4245
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-06-05 21:15:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,99 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [{
+    type: "input",
+    message: "What's your Subject or Title?",
+    name: "title",
+    validate: string => string.length > 0 ? true : "Must enter a project title."
+},
+{
+    type: "input",
+    message: "Provide a short description of your application.",
+    name: "description",
+    validate: string => string.length >= 15 ? true : "Must include a description longer than 15 characters"
+},
+{
+    type: "input",
+    message: "How will this be used? ",
+    name: "usage"
+},
+{
+    type: "input",
+    message: "Describe how to install the app",
+    name: "installation"
+},
+{
+    type: "input",
+    message: "Include the link to the demo video.",
+    name: "demo"
+},
+{
+    type: "input",
+    message: "Include a screenshot of the working application.",
+    name: "imageURL",
+},
+{
+    type: "input",
+    message: "Describe the purpose of this app and features of the app",
+    name: "features"
+},
+{
+    type: "input",
+    message: "List your collaboraters if any",
+    name: "credits"
+},
+{
+    type: "input",
+    message: "Describe how users can contribute to this application or project",
+    name: "contribute"
+},
+{
+    type: "list",
+    message: "Do you want to include a license?",
+    name: "license",
+    choices: ["MIT", "Apache", "IBM", "None"]
+},
+{
+    type: "input",
+    message: "Enter your github username",
+    name: "userName",
+    //github usernames are min. 4 char. long
+    validate: string => string.length >= 4 ? true : "Your github username can not be less than 4 characters."
+},
+{
+    type: "input",
+    message: "Provide your e-mail address: ",
+    name: "email",
+    validate: string => string.length > 0 ? true : "You must include an email"
+},
+{
+    type: "input",
+    message: "Provide your github ID:",
+    name: "createdBy"
+}];
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((response) => {
+            //if true a new img tag is created, else it is blank.
+            if (response.imageURL2 === true) {
+                response.confirmimageURL2 = `<img src="${response.confirmimageURL2}" alt="screenshot of application"/>`
+            } else {
+                response.confirmimageURL2 = "";
+            }
+            writeToFile(response)
+        })
+};
+
+//this function writes the MD file 
+function writeToFile(response) {
+    fs.writeFile(`./${response.name}README.md`, generateMarkdown(response), (err) => err ? console.error(err) : console.log('MD file created'));
+}
 
 // Function call to initialize app
 init();
